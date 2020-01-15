18cba144d7b3d22285d328868c248e4e681775c7
Evie Perren <evie.perren@simplyhealth.co.uk>
2020-01-15 15:40:04 +0000

    created two collections

--- src/database/mongoose-test.js
+++ src/database/mongoose-test.js
@@ -7,7 +7,6 @@ mongoose.connect('mongodb://localhost:27015/animals', {useNewUrlParser: true})
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error: '))
 db.once('open', function(){
-    // connected
     console.log('connected')
 
     const app = express()
@@ -18,12 +17,24 @@ db.once('open', function(){
         res.send(kitty)
         console.log(req.query)
     }) 
+    app.get('/safariZoo', function(req, res){
+        var safari = new SafariAnimal({name: req.query.safari})
+        safari.save()
+        res.send(safari)
+    })
 
+    app.get('/farmAnimals', function(req, res){
+        var fourlegs = new FarmAnimal({name: req.query.farm})
+        fourlegs.save()
+        res.send(fourlegs)
+    })
     app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-    var kittenSchema = new mongoose.Schema({
+    var animalSchema = new mongoose.Schema({
         name: String
     })
     
-    var Kitten = mongoose.model('Cats', kittenSchema)
+    var Kitten = mongoose.model('tabbies', animalSchema)
+    let SafariAnimal = mongoose.model('zoo animals', animalSchema)
+    let FarmAnimal = mongoose.model('farm animals', animalSchema)
 })
